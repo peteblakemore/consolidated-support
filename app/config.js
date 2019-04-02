05a7b820a128b7810736bd696052a40c24d827b5
Pete Blakemore <pete.blakemore@digital.education.gov.uk>
2019-04-02 15:57:51 +0100

    Contact section of homepage

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apprenticeship service help',
+  serviceName: 'Apprenticeships help',
 
   // Default port that prototype runs on
   port: '3000',
